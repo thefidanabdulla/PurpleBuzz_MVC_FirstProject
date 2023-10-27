0576358cb2f569b9803850560c9c8feff5755643
thefidanabdulla <thefidanabdulla@gmail.com>
2023-10-27 18:29:41 +0400

    first commit

---
+++ MVCProject_PurpleBuzz/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVCProject_PurpleBuzz.Models;
+
+namespace MVCProject_PurpleBuzz.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    
+}
+
---
+++ MVCProject_PurpleBuzz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCProject_PurpleBuzz.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
